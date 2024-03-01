4497dfc451a617c9d141ac0c5212d3ac82700b64
Мария <git@github.com:MariyaRass/helloworld.git>
2024-03-01 13:36:51 +0300

    дз1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] arr = generateRandomArray();
+       System.out.println("Сгенерируй массив  "+ Arrays.toString(arr));
+       task1(arr);
+       task2(arr);
+       task3(arr);
+       task4();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1(int[]arr){
+        System.out.println("Задание 1");
+        int sum = 0;
+        for(int money : arr){
+            sum=sum+money;
+        }
+        System.out.println("Сумма трат за месяц составила "+sum+" рублей");
+    }
+    public static void task2(int[]arr) {
+        System.out.println("Задание 2");
+    }
+
+    public static void task3(int[]arr) {
+        System.out.println("Задание 3");
+    }
+
+    public static void task4() {
+        System.out.println("Задание 4");
     }
 }
+
+
+
